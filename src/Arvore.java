42fdf8b7b0f624f45a37d9cadbeaafb4d7ce9c25
Eduardo Quinalha <eduardoquinalha@gmail.com>
2022-11-13 20:21:48 -0300

    Commit Inicial

---
+++ src/Arvore.java
@@ -0,0 +1,12 @@
+
+public class Arvore {
+	private No raiz;
+	
+	public void insere(Integer valor) {
+		insere(raiz, valor);
+	}
+	
+	public void insere(No no, Integer valor) {
+		
+	}
+}
---
+++ src/No.java
@@ -0,0 +1,37 @@
+
+public class No {
+	private Integer valor;
+	private No esquerda;
+	private No direita;
+	
+	public No(Integer valor) {
+		super();
+		this.valor = valor;
+	}
+
+	public Integer getValor() {
+		return valor;
+	}
+
+	public void setValor(Integer valor) {
+		this.valor = valor;
+	}
+
+	public No getEsquerda() {
+		return esquerda;
+	}
+
+	public void setEsquerda(No esquerda) {
+		this.esquerda = esquerda;
+	}
+
+	public No getDireita() {
+		return direita;
+	}
+
+	public void setDireita(No direita) {
+		this.direita = direita;
+	}
+	
+	
+}
